a134645795377bad30624518bf1fd46cd39b5db2
V3ronique <veronica.velkova@gmail.com>
2016-01-08 00:42:35 +0200

    Second view + an event added

---
+++ MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/LevelDifficulty.java
@@ -0,0 +1,13 @@
+package com.example.willie_the_coyote.beep_beep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_difficulty);
+    }
+}
--- MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.willie_the_coyote.beep_beep;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.willie_the_coyote.beep_beep.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loadLevelDifficulty(View view) {
+        Intent intent;
+        intent = new Intent(this, LevelDifficulty.class);
+       startActivity(intent);
     }
 }
