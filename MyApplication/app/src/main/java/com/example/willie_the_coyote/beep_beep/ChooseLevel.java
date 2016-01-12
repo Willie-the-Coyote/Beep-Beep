6cce7e26e31f3681202cbdedfdc8e9dd1cb7704b
V3ronique <veronica.velkova@gmail.com>
2016-01-12 16:45:20 +0200

    Added new view with buttons

--- MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/ChooseLevel.java
+++ MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/ChooseLevel.java
@@ -1,7 +1,9 @@
 package com.example.willie_the_coyote.beep_beep;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChooseLevel extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class ChooseLevel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_level);
+        TextView tv=(TextView)findViewById(R.id.tv_attempt);
+        Typeface face=Typeface.createFromAsset(getAssets(), "font/cactus_font.ttf");
+        tv.setTypeface(face);
     }
 }
--- MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/LevelDifficulty.java
+++ MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/LevelDifficulty.java
@@ -1,7 +1,9 @@
 package com.example.willie_the_coyote.beep_beep;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LevelDifficulty extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LevelDifficulty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_difficulty);
+    }
+
+    public void loadChooseLevel(View view){
+        Intent intent;
+        intent = new Intent(this, ChooseLevel.class);
+        startActivity(intent);
     }
 }
