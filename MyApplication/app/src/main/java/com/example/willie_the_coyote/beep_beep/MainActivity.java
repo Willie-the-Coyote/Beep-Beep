2c45a36ef77c3b9c807175a40d96d025942fd94d
V3ronique <veronica.velkova@gmail.com>
2016-01-13 14:52:48 +0200

    Changed font color

--- MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/willie_the_coyote/beep_beep/MainActivity.java
@@ -18,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
     public void loadLevelDifficulty(View view) {
         Intent intent;
         intent = new Intent(this, LevelDifficulty.class);
-       startActivity(intent);
+        startActivity(intent);
     }
 }
